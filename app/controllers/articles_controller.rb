98d4e3c3ebf9bab04ef6dc94e41a733cc00aa729
ruby <rubynichols2010@gmail.com>
2014-09-06 20:45:41 -0700

    third commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,22 +2,20 @@ class ArticlesController < ApplicationController
 
  def new
   end
+
 def create
   @article = Article.new(article_params)
+
+  if @article.save
+    redirect_to @article
+  else
+    render 'new'
+  end
+end
  
-  @article.save
-  redirect_to @article
-end
-
-
- def show
-  @article = Article.find(params[:id])
-end
-
 
 private
   def article_params
     params.require(:article).permit(:title, :text)
   end
-
- end
+end
